132ae669204f88ba2a3a19b73f7cc0404f4e8f1b
Peter Berkenbosch <info@peterberkenbosch.nl>
2025-07-21 22:18:24 +0200

    Using phlex_icon helper in Phlex View sample.

--- app/views/home/index.rb
+++ app/views/home/index.rb
@@ -3,6 +3,7 @@
 class Views::Home::Index < Views::Base
   def view_template
     h1 { "Home::Index" }
+    div { phlex_icon "hero/home", variant: :solid, class: "w-5 h-5 text-blue-500" }
     div do
       Bootstrap::House(class: "size-4")
       Flag::Nl(variant: :rectangle, class: "size-4")
