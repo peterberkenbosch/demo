d213cac866930664987bc73a7e60fc8e9f2e8010
Peter Berkenbosch <info@peterberkenbosch.nl>
2025-07-21 22:13:20 +0200

    Re-generated Phlex config

--- app/components/base.rb
+++ app/components/base.rb
@@ -1,17 +1,13 @@
 # frozen_string_literal: true
 
 class Components::Base < Phlex::HTML
-  include Components
-
   # Include any helpers you want to be available across all components
   include Phlex::Rails::Helpers::Routes
-  include PhlexIcons
 
   if Rails.env.development?
     def before_template
       comment { "Before #{self.class.name}" }
       super
-      comment { "After #{self.class.name}" }
     end
   end
 end
